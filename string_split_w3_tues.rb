b19ffb0542475350f4cd3cbac6caa88a1aa807f9
mkg1 <gehring.mg@gmail.com>
2016-05-01 21:42:55 -0400

    Refactor string split challenge

--- string_split_w3_tues.rb
+++ string_split_w3_tues.rb
@@ -8,24 +8,35 @@ require 'minitest/pride'
 
 # WRITE YOUR CODE HERE.
 #one way
-def first_name(string)
-  arrayed = string.split
-  if arrayed.length > 2
-    return arrayed[0..-2].join(" ")
-  elsif arrayed.length == 2 || arrayed.length == 1
-    return arrayed[0]
-  else
-    return ""
-  end
+# def first_name(string)
+#   arrayed = string.split
+#   if arrayed.length > 2
+#     return arrayed[0..-2].join(" ")
+#   elsif arrayed.length == 2 || arrayed.length == 1
+#     return arrayed[0]
+#   else
+#     return ""
+#   end
+# end
+#
+# def last_name(string)
+#   arrayed = string.split
+#   if arrayed.length >= 2
+#     return arrayed[-1]
+#   else
+#     return ""
+#   end
+# end
+
+#another way
+def first_name(name)
+  arrayed = name.split
+  arrayed.length > 1 ? arrayed[0..-2].join(" ") : arrayed.join("")
 end
 
-def last_name(string)
-  arrayed = string.split
-  if arrayed.length >= 2
-    return arrayed[-1]
-  else
-    return ""
-  end
+def last_name(name)
+  arrayed = name.split
+  arrayed.length > 1 ? arrayed[-1] : ""
 end
 
 class StringSplitChallenge < MiniTest::Test
